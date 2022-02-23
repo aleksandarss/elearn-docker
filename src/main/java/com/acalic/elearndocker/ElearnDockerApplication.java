4f86c4a15e3291400139b6a0925755e6a733e2e1
Aleksandars991 <alexcsasha91@gmail.com>
2022-02-23 22:51:45 +0100

    initial commit

---
+++ src/main/java/com/acalic/elearndocker/ElearnDockerApplication.java
@@ -0,0 +1,13 @@
+package com.acalic.elearndocker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElearnDockerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ElearnDockerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/acalic/elearndocker/ElearnDockerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.acalic.elearndocker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ElearnDockerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
